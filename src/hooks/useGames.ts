102f06800e80e2f48a72053cc196b21921ae57c1
David Graham <dagraham7@gmail.com>
2023-12-04 22:26:31 -0600

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,10 +7,13 @@ import { Platform } from "./usePlatforms";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  description_raw: string;
+  description: string;
 }
 
 const apiClient = new APIClient<Game>("/games");
