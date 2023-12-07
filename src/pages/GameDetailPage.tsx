d8dd2c066fa879339c5b00246a205bc9aea6f429
David Graham <dagraham7@gmail.com>
2023-12-06 21:49:20 -0600

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { Spinner } from "@chakra-ui/spinner";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
